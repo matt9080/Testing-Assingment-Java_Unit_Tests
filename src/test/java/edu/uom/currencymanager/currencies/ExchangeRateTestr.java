b329fa13a89bf9bd3decc3802adec01a9aa34828
Matthew Cachia <cachiamatthew1998@gmail.com>
2019-11-10 09:28:07 +0100

    Pls Ignore need to switch computer
    Non Major commit.

--- src/test/java/edu/uom/currencymanager/currencies/ExchangeRateTestr.java
+++ src/test/java/edu/uom/currencymanager/currencies/ExchangeRateTestr.java
@@ -2,14 +2,32 @@ package edu.uom.currencymanager.currencies;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class ExchangeRateTestr {
 
-//    @Test
-    public void toStringSuccessTest(){
 
-}
 
-    public static void main (String args[]){
 
+    final String CURR_CODE = "TES";
+    final String CURR_NAME = "TESTING";
+    final boolean CURR_TRUE = true;
+    final String CURR_CODE_2 = "YEE";
+    final String CURR_NAME_2 = "YEETING";
+    final boolean CURR_FALSE = false;
+    final double RATE = 0.42;
+
+    @Test
+    public void toStringSuccessTest() {
+        Currency x = new Currency(CURR_CODE,CURR_NAME, CURR_TRUE);
+        Currency y = new Currency(CURR_CODE_2,CURR_NAME_2, CURR_TRUE);
+        ExchangeRate exchangeRate = new ExchangeRate(x,y,RATE);
+        assertEquals(CURR_CODE +" 1 = "+ CURR_CODE_2 + " " + RATE, exchangeRate.toString());
     }
+
+    @Test
+    public void exchangeRateCreationTest() {
+        ExchangeRate exchangeRate = new ExchangeRate();
+    }
+
 }
