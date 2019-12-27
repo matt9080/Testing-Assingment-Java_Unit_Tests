dba5f49837acd1dd8b48393371f29fafd9598aa1
Matthew Cachia <cachiamatthew1998@gmail.com>
2019-12-27 09:41:30 +0100

    Added Add & Delete menu + methods

--- src/main/java/edu/uom/currencymanager/Menu.java
+++ src/main/java/edu/uom/currencymanager/Menu.java
@@ -1,5 +1,6 @@
 package edu.uom.currencymanager;
 
+import edu.uom.currencymanager.currencies.Currency;
 import edu.uom.currencymanager.currencies.CurrencyDatabase;
 
 import java.util.Scanner;
@@ -65,3 +66,64 @@ public class Menu {
             }
         }
     }
+
+    public void deleteCurrency_menu(){
+        System.out.print("\nEnter the currency code: ");
+        String code = sc.next().toUpperCase();
+        try {
+            deleteCurrency(code);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void deleteCurrency(String code) throws Exception {
+        if (!currencyDatabase.currencyExists(code)) {
+            throw new Exception("Currency does not exist: " + code);
+        }
+
+        currencyDatabase.deleteCurrency(code);
+    }
+
+    public void addCurrency_menu(){
+        System.out.print("\nEnter the currency code: ");
+        String code = sc.next().toUpperCase();
+        System.out.print("Enter currency name: ");
+        String name = sc.next();
+        name += sc.nextLine();
+
+        String major = "\n";
+        while (!(major.equalsIgnoreCase("y") || major.equalsIgnoreCase("n"))) {
+            System.out.println("Is this a major currency? [y/n]");
+            major = sc.next();
+        }
+
+        try {
+            addCurrency(code, name, major.equalsIgnoreCase("y"));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+    }
+
+    public void addCurrency(String code, String name, boolean major) throws Exception {
+
+        //Check format of code
+        if (code.length() != 3) {
+            throw new Exception("A currency code should have 3 characters.");
+        }
+
+        //Check minimum length of name
+        if (name.length() < 4) {
+            throw new Exception("A currency's name should be at least 4 characters long.");
+        }
+
+        //Check if currency already exists
+        if (currencyDatabase.currencyExists(code)) {
+            throw new Exception("The currency " + code + " already exists.");
+        }
+        //Add currency to database
+        currencyDatabase.addCurrency(new Currency(code,name,major));
+
+    }
+}
